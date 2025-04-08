610b83203f4c694bace84e3b3df227876a6f6aba
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-04-08 12:37:08 +0600

    Project created and top navbar created

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Archivo } from "next/font/google";
+import "./globals.css";
+
+const archivo = Archivo({
+  display: "swap",
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-archivo",
+});
+
+export const metadata: Metadata = {
+  title: "Minimal Single Page Portfolio",
+  description: "Crafting digital experiences through code and creative design",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased bg-stone-200 text-stone-900 font-sans ${archivo.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
