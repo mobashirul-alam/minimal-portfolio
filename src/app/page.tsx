d5f81acd1ebc519002b7bedfe5247d915084c5b5
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-08-08 13:00:59 +0600

    Footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import FAQs from "@/sections/FAQs";
+import Footer from "@/sections/Footer";
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
 import Intro from "@/sections/Intro";
 import Projects from "@/sections/Projects";
-import Testimonials from "@/sections/Testimonials";
 
 export default function Home() {
   return (
@@ -12,8 +12,9 @@ export default function Home() {
       <Hero />
       <Intro />
       <Projects />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <FAQs />
+      <Footer />
     </>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,56 @@
+import Button from "@/components/Button";
+import { iconArrowUpRight } from "@/utils/myIcons";
 import { FC } from "react";
 
 const Footer: FC = () => {
-  return <div>Footer</div>;
+  const navItems = [
+    { href: "#", label: "Home" },
+    { href: "#", label: "Projects" },
+    { href: "#", label: "Testimonials" },
+    { href: "#", label: "FAQs" },
+    { href: "#", label: "Contact" },
+  ];
+
+  return (
+    <footer className="bg-stone-900 text-white">
+      <div className="container">
+        <div className="section">
+          <div className="flex items-center gap-3">
+            <div className="size-3 rounded-full bg-green-400"></div>
+            <span className="uppercase">One spot available for next month</span>
+          </div>
+          <div className="grid md:grid-cols-3 md:items-center">
+            <div className="md:col-span-2">
+              <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight">
+                Enough talk. Let&apos;s build something together
+              </h2>
+              <Button
+                variant="secondary"
+                iconAfter={iconArrowUpRight}
+                className="mt-8"
+              >
+                info@anderson.com
+              </Button>
+            </div>
+            <div>
+              <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
+                {navItems.map(({ href, label }) => (
+                  <a href={href} key={label}>
+                    <Button variant="text" className="text-lg">
+                      {label}
+                    </Button>
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
+        </div>
+        <p className="py-16 text-white/30 text-sm">
+          Copyright &copy; Anderson &bull; All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
