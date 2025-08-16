6a15d0597297c4f3beb9121e1c7bbb0a7b076eb7
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-08-16 23:51:51 +0600

    header animation added, and smooth behavior added

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -2,7 +2,10 @@ import { FC } from "react";
 
 const Intro: FC = () => {
   return (
-    <section className="py-24 md:py-32 lg-py-40 mt-12 md:mt-16 lg:mt-20">
+    <section
+      className="py-24 md:py-32 lg-py-40 mt-12 md:mt-16 lg:mt-20"
+      id="intro"
+    >
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%]">
           Building beautiful websites with clean code and thoughtful design to
