4eb1460a14c05a153349e71e54b382b0ced32f25
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-08-19 10:03:26 +0600

    Enhance Intro section with animation effects and refactor SVG icon properties for consistency

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,10 +1,42 @@
-import { FC } from "react";
+"use client";
+
+import { stagger, useAnimate, useInView } from "motion/react";
+import { FC, useEffect } from "react";
+import SplitType from "split-type";
 
 const Intro: FC = () => {
+  const [introScope, introAnimate] = useAnimate();
+  const inView = useInView(introScope, {
+    once: true,
+  });
+
+  useEffect(() => {
+    new SplitType(introScope.current.querySelector("h2"), {
+      types: "lines,words",
+      tagName: "span",
+    });
+  }, [introScope]);
+
+  useEffect(() => {
+    if (inView) {
+      introAnimate(
+        introScope.current.querySelectorAll(".word"),
+        {
+          transform: "translateY(0)",
+        },
+        {
+          duration: 0.5,
+          delay: stagger(0.18),
+        }
+      );
+    }
+  }, [inView, introAnimate, introScope]);
+
   return (
     <section
       className="py-24 md:py-32 lg-py-40 mt-12 md:mt-16 lg:mt-20"
       id="intro"
+      ref={introScope}
     >
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%]">
--- src/utils/myIcons.js
+++ src/utils/myIcons.js
@@ -3,13 +3,13 @@ export const iconArrowUpRight = (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="currentColor"
     className="size-6"
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
     />
   </svg>
